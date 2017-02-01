44882420cdd3761357d423528722358c6eb0a5f2
JelenaSret <jelena032.js@gmail.com>
2017-02-01 02:10:38 -0800

    Initial checkin of sales tool

---
+++ SalesTool/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SalesData data= new SalesData();
+		displayGreeting();
+		data.display();
+	}
+
+	private static void displayGreeting() {
+		// TODO Auto-generated method stub
+		System.out.println("This is app sales data");
+	}
+}
---
+++ SalesTool/src/SalesData.java
@@ -0,0 +1,10 @@
+
+public class SalesData {
+	int data [] = {0, 4 , 42};
+	public void display() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("implement display method");
+	}
+
+}
